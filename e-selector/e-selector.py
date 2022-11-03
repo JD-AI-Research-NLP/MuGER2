98178debb78cd368b89b99928cd45a64c1f56b43
yywang0514 <34204667+yywang0514@users.noreply.github.com>
2022-11-03 14:30:55 +0800

    Update e-selector.py

--- e-selector/e-selector.py
+++ e-selector/e-selector.py
@@ -18,7 +18,6 @@ import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from transformers import BertTokenizer, BertModel
 import copy
 import torch.optim as optim
@@ -30,9 +29,7 @@ from train_mrc_base import ModelDefine as MRC_base
     
 class E_SELECTOR:
     def __init__(self, lr=5e-5, device=None):
-
-        self.flag = 0
-               
+        self.flag = 0               
     def selector(self, mode, mrc_mode, retrieval_score_path, prediction_path):
         sigmoid_func = nn.Sigmoid()
         softmax_func = nn.Softmax(-1)
@@ -63,38 +60,30 @@ class E_SELECTOR:
         elif mode == 'test':
             with open('../../processed_data/hop_location_test.json') as f:
                 hop_location = json.load(f)            
-        answer4dev = []
-      
+        answer4dev = []      
         with open(retrieval_score_path) as f:
             dev_questions = json.loads(f.read())
-
         correct_question_or = 0
-
         correct_header = 0
         correct_anchor = 0
         correct_hop = 0
         correct_answer = 0
-        correct_link = 0       
-        
+        correct_link = 0               
         have_header_q = 0
         have_anchor_q = 0
         have_hop_q = 0
         have_answer_q = 0
-        have_link_q = 0
-     #   ipdb.set_trace()
+        have_link_q = 0)
         for item in tqdm(all_questions): 
             qid = item['question_id']
             cur_hop_location = hop_location[qid]
-            cur_answer={}
-            
+            cur_answer={}          
             with open('../../processed_data/WikiTables-WithLinks/request_tok/{}.json'.format(item['table_id'])) as f:
                 all_passage = json.load(f) 
             with open('../../processed_data/WikiTables-WithLinks/tables_tok/{}.json'.format(item['table_id'])) as f:
-                table = json.load(f) 
-      
+                table = json.load(f)     
             column_len = len(table['data'][0])
-            data = dev_questions[qid]
-            
+            data = dev_questions[qid]           
             if sum(data['header_label']) != 0:
                 have_header_q+=1
             if sum(data['anchor_label']) != 0:
@@ -104,43 +93,33 @@ class E_SELECTOR:
             if sum(data['answer_label']) != 0:
                 have_answer_q+=1
             if sum(data['link_label']) != 0:
-                have_link_q+=1
-                
-         
+                have_link_q+=1                         
             anchor_score = np.array(data['anchor_score']).reshape(-1,column_len).tolist()
             answer_score = np.array(data['answer_score']).reshape(-1,column_len).tolist()
-            # get global answer score
-      
+            # get global answer score     
             new_answer_score = []
             for i, row_score in enumerate(answer_score):
                 cur_row_score = []
                 for j, cell_score in enumerate(row_score):
                     new_cell_score = cell_score + data['header_score'][j] + max(anchor_score[i])
                     cur_row_score.append(new_cell_score)
-                new_answer_score.append(cur_row_score)
-      
+                new_answer_score.append(cur_row_score)      
             new_answer_score = np.array(new_answer_score).reshape(-1).tolist()
             max_answer_score = max(new_answer_score)
             max_answer_score_id = new_answer_score.index(max_answer_score)
             max_answer_context = data['answer_context'][max_answer_score_id]
             max_answer_linktitle = data['answer_linktitle'][max_answer_score_id]
-            # get global hop score
-       
+            # get global hop score       
             new_hop_score = []
             for hs, hloc in zip(data['hop_score'], cur_hop_location):
-                new_hop_score.append(hs + max(anchor_score[hloc[0]]) + data['header_score'][hloc[1]])
-        
+                new_hop_score.append(hs + max(anchor_score[hloc[0]]) + data['header_score'][hloc[1]])        
             max_hop_score = max(new_hop_score)
             max_hop_score_id = new_hop_score.index(max_hop_score)
             max_hop_context = data['hop_context'][max_hop_score_id]
             max_hop_linktitle = data['hop_linktitle'][max_hop_score_id]
-
-            if max_answer_score >= max_hop_score:    
-           
-                    ans = max_answer_context
-       
-            else:
-                    
+            if max_answer_score >= max_hop_score:               
+                    ans = max_answer_context       
+            else:                    
                     cur_hop_link_c = []
                     cur_hop_link_s = []
                     cur_hop_link_l = []
@@ -149,14 +128,10 @@ class E_SELECTOR:
                         if link_c in cur_hop_link:
                             cur_hop_link_s.append(link_s)
                             cur_hop_link_c.append(link_c)
-                            cur_hop_link_l.append(link_l)
-                         
-                    #用最大link
+                            cur_hop_link_l.append(link_l)                         
                     max_cur_hop_link_index = cur_hop_link_s.index(max(cur_hop_link_s)) 
-                    max_cur_hop_link_label = cur_hop_link_l[max_cur_hop_link_index]
-                    
+                    max_cur_hop_link_label = cur_hop_link_l[max_cur_hop_link_index]                    
                     max_cur_hop_link_c = cur_hop_link_c[max_cur_hop_link_index]
-
                     passage_tok = bert_tokenizer.tokenize(all_passage[max_cur_hop_link_c])
                     question_tok = bert_tokenizer.tokenize(item['question'])
                     tokens = ['[CLS]'] + question_tok + ['[SEP]'] + passage_tok
@@ -167,10 +142,8 @@ class E_SELECTOR:
                         input_type_ids = input_type_ids[0:512]
                     input_ids = torch.from_numpy(np.array(input_ids)).long().to('cuda').unsqueeze(0)
                     input_type_ids = torch.from_numpy(np.array(input_type_ids)).to('cuda').unsqueeze(0)
-                    attention_mask = (input_ids > 0.5).long().to('cuda')                      
-                        
-                    start_logits, end_logits = mrc_model(input_ids, input_type_ids,attention_mask)
-                        
+                    attention_mask = (input_ids > 0.5).long().to('cuda')                                              
+                    start_logits, end_logits = mrc_model(input_ids, input_type_ids,attention_mask)                       
                     start_prob = softmax_func(start_logits.squeeze(-1).squeeze(0)).detach().cpu().numpy().tolist()
                     end_prob = softmax_func(end_logits.squeeze(-1).squeeze(0)).detach().cpu().numpy().tolist()
                     span_pos = []
@@ -179,18 +152,14 @@ class E_SELECTOR:
                                 for j,e in enumerate(end_prob):
                                     if i<=j:
                                         span_score.append(s*e)
-                                        span_pos.append([i,j])
-        
+                                        span_pos.append([i,j])        
                     span_idx = np.array(span_score).argsort()[::-1]
                     span_pos = np.array(span_pos)[span_idx].tolist()
-                    for pos in span_pos:
-                    
+                    for pos in span_pos:                    
                                 sp = pos[0]
                                 ep = pos[1]
-                                if sp <= ep:
-                          
+                                if sp <= ep:                          
                                     answer = tokens[sp:ep+1]
-                                
                                     final_answer = []
                                     word_begin = len(final_answer)-1
                                     for i,w in enumerate(answer):
@@ -204,12 +173,10 @@ class E_SELECTOR:
                                             else:
                                                 final_answer[word_begin] = final_answer[word_begin] + w.replace('##','')
                                     ans = ' '.join(final_answer)
-                                    ans = ans.replace(' . ','.').replace(' , ',',')
-                                
+                                    ans = ans.replace(' . ','.').replace(' , ',',')                                
                                     break
             if ans == ' ':
                 ans = max_answer_context
-
             header_index = data['header_score'].index(max(data['header_score']))
             anchor_index = data['anchor_score'].index(max(data['anchor_score']))
             hop_index = data['hop_score'].index(max(data['hop_score']))
@@ -227,26 +194,18 @@ class E_SELECTOR:
                 correct_link += 1
             if data['hop_label'][max_hop_score_id] == 1 or data['answer_label'][max_answer_score_id] == 1:
                 correct_question_or += 1
-
             cur_answer['question_id'] = qid
-            cur_answer['pred'] = ans
-           
+            cur_answer['pred'] = ans          
             answer4dev.append(cur_answer)
-
-        print(len(answer4dev)) 
-        
+        print(len(answer4dev))        
         with open(prediction_path,'w') as f:
-            f.write(json.dumps(answer4dev))
-  
-     
-        
+            f.write(json.dumps(answer4dev))        
         print('have header q num = {}'.format(have_header_q))
         print('have anchor q num = {}'.format(have_anchor_q))
         print('have hop q num = {}'.format(have_hop_q))
         print('have answer q num = {}'.format(have_answer_q))
         print('have link q num = {}'.format(have_link_q))
-        print('============================================')
-    
+        print('============================================')    
         print('correct header num = {}'.format(correct_header))
         print("header acc={}".format(correct_header/have_header_q))
         print('correct anchor num = {}'.format(correct_anchor))
@@ -257,13 +216,10 @@ class E_SELECTOR:
         print("answer acc={}".format(correct_answer/have_answer_q))
         print('correct link num = {}'.format(correct_link))
         print("link acc={}".format(correct_link/have_link_q))
-        print('correct question num or = {}'.format(correct_question_or))
-  
-        print("question acc or={}".format(correct_question_or/len(dev_questions)))
-   
-    
-
-    def save(self, filename, epoch):
+        print('correct question num or = {}'.format(correct_question_or)) 
+        print("question acc or={}".format(correct_question_or/len(dev_questions)))   
+        
+    def save(self, filename, epoch):       
         params = {
             'state_dict': {
                 'network': self.model.state_dict(),
@@ -274,28 +230,23 @@ class E_SELECTOR:
         }
         torch.save(params, filename)
 
-    def resume(self, filename):
-        
+    def resume(self, filename):        
         checkpoint = torch.load(filename)
         state_dict = checkpoint['state_dict']
         new_state = {}
         for key in state_dict['network'].keys():
             new_key = key.replace('module.','')
             new_state[new_key] = state_dict['network'][key]   
-
         self.model.load_state_dict(new_state)
         self.model.to('cuda')
         return self.model
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", type=str, default='dev')
     parser.add_argument("--mrc_mode", type=str, default="large")
     parser.add_argument("--input_path", type=str, default='./retrieval_score_large.json')
-    parser.add_argument("--output_path", type=str, default="./predictions_large.json")
-    
-   
+    parser.add_argument("--output_path", type=str, default="./predictions_large.json")    
     args = parser.parse_args()
     synonym_model = E_SELECTOR()
     synonym_model.selector(args.mode, args.mrc_mode, args.input_path, args.output_path)
